de3512ede931a9c0f79e932aa711762fdd788c0e
helton-evambi <heltonevambi@gmail.com>
2025-03-24 22:57:15 +0100

    feat: create GetProducts features

---
+++ src/Sevices/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+using OpenTelemetry.Trace;
+
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var results = await sender.Send(new GetProductsQuery());
+
+            var response = results.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Product");
+    }
+}

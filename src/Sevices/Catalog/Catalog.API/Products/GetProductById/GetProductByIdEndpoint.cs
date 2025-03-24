2da032cab09408ac4732a2bc43156c57bd608f6d
helton-evambi <heltonevambi@gmail.com>
2025-03-24 22:57:02 +0100

    feat: create GetProductById features

---
+++ src/Sevices/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : CarterModule
+{
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products{id}", (Guid id, ISender sender) =>
+        {
+            var result = sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}

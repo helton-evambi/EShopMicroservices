3c4b379034c727e7c53e8343d0f566aa11d6ac7a
helton-evambi <heltonevambi@gmail.com>
2025-04-14 08:50:40 +0100

    feat: configure global exception pipeline

--- src/Sevices/Catalog/Catalog.API/Program.cs
+++ src/Sevices/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,7 @@
+using BuidingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to container
@@ -6,7 +10,10 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(connfig =>
 {
     connfig.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    connfig.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 builder.Services.AddMarten(opts =>
 {
@@ -18,5 +25,32 @@ var app = builder.Build();
 // Configure the Http request pipeline
 
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if(exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();

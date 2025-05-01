b1504496a978926bc4ecafac8b930013b83788cf
helton-evambi <heltonevambi@gmail.com>
2025-05-01 19:59:25 +0100

    feat: configure marten library

--- src/Sevices/Basket/Basket.API/GlobalUsing.cs
+++ src/Sevices/Basket/Basket.API/GlobalUsing.cs
@@ -5,3 +5,6 @@ global using MediatR;
 global using Mapster;
 global using FluentValidation;
 global using BuidingBlocks.Behaviors;
+global using BuidingBlocks.Exceptions;
+global using Basket.API.Exceptions;
+global using Marten;

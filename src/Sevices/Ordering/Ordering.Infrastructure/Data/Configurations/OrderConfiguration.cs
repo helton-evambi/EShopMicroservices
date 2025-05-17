994d9f896e12736950a82ccfc0aa05248d9c26ff
helton-evambi <heltonevambi@gmail.com>
2025-05-17 12:18:09 +0100

    refactor: using directives to global scope

--- src/Sevices/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ src/Sevices/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Enums;
-using Ordering.Domain.Models;
-using Ordering.Domain.ValueObjects;
+﻿using Ordering.Domain.Enums;
 
 namespace Ordering.Infrastructure.Data.Configurations;
 
--- src/Sevices/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
+++ src/Sevices/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Models;
-
 namespace Ordering.Infrastructure.Data.Configurations;
 
 public class ProductConfiguration : IEntityTypeConfiguration<Product>

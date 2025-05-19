a3c17fc31e84e3c9b795cccb76fde9216cb1ee6b
helton-evambi <heltonevambi@gmail.com>
2025-05-19 20:00:37 +0100

    feat: enhance dependency injection with MediatR support

--- src/Sevices/Ordering/Ordering.Application/DepndencyInjection.cs
+++ src/Sevices/Ordering/Ordering.Application/DepndencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application;
 
@@ -7,6 +8,11 @@ public static class DepndencyInjection
 {
     public static IServiceCollection AddApplicationServices (this IServiceCollection services)
     {
+        services.AddMediatR(cfg => 
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
         return services;
     }
 }

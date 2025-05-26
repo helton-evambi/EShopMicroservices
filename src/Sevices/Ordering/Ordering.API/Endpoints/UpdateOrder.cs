3e992ace0fd8ca95fe412b20cdff0c9fb1924e7b
helton-evambi <heltonevambi@gmail.com>
2025-05-26 23:01:30 +0100

    feat: add update order feature to Ordering API

---
+++ src/Sevices/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -0,0 +1,28 @@
+﻿using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.API.Endpoints;
+
+public record UpdateOrderRequest(OrderDto Order);
+public record class UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateOrderCommand>();
+         
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateOrder")
+        .WithSummary("Update an existing order")
+        .WithDescription("Updates an existing order in the system.")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}

a53240ff0371e8f6d754cb2b37fe2380a4d8b579
helton-evambi <heltonevambi@gmail.com>
2025-05-26 13:29:52 +0100

    feat: add CreateOrder command and handler implementation

---
+++ src/Sevices/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order)
+    : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order).NotNull();
+        RuleFor(x => x.Order.OrderName).NotEmpty().MaximumLength(100).WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("Customer Id is required");
+        RuleForEach(x => x.Order.OrderItems).NotEmpty().WithMessage("Order Itens shoud not be null");
+    }
+}
+

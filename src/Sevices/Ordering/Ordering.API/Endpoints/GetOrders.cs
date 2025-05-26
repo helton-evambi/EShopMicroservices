5664c67ea4935f09ba4dced44a667234a68e8955
helton-evambi <heltonevambi@gmail.com>
2025-05-26 22:59:59 +0100

    feat: add GET endpoint for retrieving paginated orders

---
+++ src/Sevices/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,29 @@
+﻿using BuidingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var query = new GetOrdersQuery(request);
+
+            var result = await sender.Send(query);
+
+            var response = result.Adapt<GetOrdersResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .WithSummary("Get all orders")
+        .WithDescription("Retrieves all orders in the system with pagination.")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}

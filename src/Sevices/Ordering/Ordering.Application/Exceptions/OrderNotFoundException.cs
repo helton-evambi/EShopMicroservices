ad2f5657e1292996d05dc5c915b0beb82399a5b0
helton-evambi <heltonevambi@gmail.com>
2025-05-26 13:35:43 +0100

    feat: add OrderNotFoundException for order processing

---
+++ src/Sevices/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using BuidingBlocks.Exceptions;
+
+namespace Ordering.Application.Exceptions;
+
+public class OrderNotFoundException : NotFoundException
+{
+    public OrderNotFoundException(Guid Id) : base("base", Id)
+    {
+    }
+}

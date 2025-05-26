ed1278bc585d2083bde80f57b4e1da3bf7ccfab7
helton-evambi <heltonevambi@gmail.com>
2025-05-26 23:00:32 +0100

    feat: add endpoint to retrieve orders by customer ID

---
+++ src/Sevices/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,28 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+public class GetOrdersByCustomer : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId:guid}", async (Guid customerId, ISender sender) =>
+        {
+            var query = new GetOrdersByCustomerQuery(customerId);
+
+            var result = await sender.Send(query);
+
+            var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .WithSummary("Get orders by customer ID")
+        .WithDescription("Retrieves all orders associated with a specific customer ID.")
+        .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
+

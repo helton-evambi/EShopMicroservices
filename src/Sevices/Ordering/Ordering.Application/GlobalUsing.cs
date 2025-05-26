190db9f7a0556408adf2e5ba09bb7e743f9b38a6
helton-evambi <heltonevambi@gmail.com>
2025-05-26 13:36:48 +0100

    feat: add global using directives for Ordering application

---
+++ src/Sevices/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using BuidingBlocks.CQRS;
+global using FluentValidation;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;

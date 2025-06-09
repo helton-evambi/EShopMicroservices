978043189a65d49a75890727d5495e8310d09221
helton-evambi <heltonevambi@gmail.com>
2025-06-09 09:44:56 +0100

    feat: add BasketCheckoutEvent and IntegrationEvent records

---
+++ src/BuidingBlocks/BuidingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuidingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OcocurredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}

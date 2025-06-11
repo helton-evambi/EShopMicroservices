9ceec6f03f8dfffb636c159f89cd44504a295ea7
helton-evambi <heltonevambi@gmail.com>
2025-06-11 21:03:36 +0100

    feat: add feature flag for order creation event publishing

--- src/Sevices/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Sevices/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,17 +1,20 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
 public class OrderCreatedEventHandler
-    (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handler: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIngratinEvent = domainEvent.Order.ToOrderDto();
-
-        await publishEndpoint.Publish(orderCreatedIngratinEvent, cancellationToken);
+        if(await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            var orderCreatedIngratinEvent = domainEvent.Order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIngratinEvent, cancellationToken);
+        }
     }
 }

afe3af41e1c6d9bebb224ff3d64b5d20ab964a70
helton-evambi <heltonevambi@gmail.com>
2025-06-23 09:06:52 +0100

    feat: implement IOrderingService interface with order retrieval methods

---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,17 @@
+﻿using Shopping.Web.Models.Ordering;
+
+namespace Shopping.Web.Services;
+
+public interface IOrderingService
+{
+    [Get("/ordering-service/orders?pageIndex={pagaIndex}&pageSize={pageSize}\"")]
+    Task<GetOrdersResponse> GetOrders(int? pageNumber = 1, int? pageSize = 10);
+
+    [Get("/ordering-service/orders/name/{orederName}")]
+    Task<GetOrdersByNameResponse> GetOrdersByName(string orederName);
+
+    [Get("/ordering-service/orders/customer/{customerId}")]
+    Task<GetOrdersByCustomerResponse> GetOrdersByCustomer(Guid customerId);
+
+    
+}

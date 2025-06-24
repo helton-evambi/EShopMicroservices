fba7879548f109acec1b4a43a295aee15a39477f
helton-evambi <heltonevambi@gmail.com>
2025-06-24 16:14:28 +0100

    refactor: update IndexModel constructor to include IBasketService and clean up method structure

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,16 +1,37 @@
-namespace Shopping.Web.Pages
-{
-    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger)
-        : PageModel
-    {
-        public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+namespace Shopping.Web.Pages;
 
-        public async Task<IActionResult> OnGetAsync()
+public class IndexModel(ICatalogService catalogService, IBasketService basketService, ILogger<IndexModel> logger)
+    : PageModel
+{
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        logger.LogInformation("Index page visited.");
+        var response = await catalogService.GetProducts();
+        ProductList = response.Products;
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
         {
-            logger.LogInformation("Index page visited.");
-            var response = await catalogService.GetProducts();
-            ProductList = response.Products;
-            return Page();
-        }
+            ProductId = productResponse.Product.Id,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
     }
 }
